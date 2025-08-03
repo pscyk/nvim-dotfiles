014acf8de012ada67981cd7ad4491b02cb5ee923
C Y K Sagar <cyksagar@protonmail.com>
2025-08-04 05:22:27 +0530

    update file picker

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -52,7 +52,7 @@ return {
     vim.keymap.set('n', '<leader>/', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
     vim.keymap.set('n', '<leader><space>', require('telescope.builtin').current_buffer_fuzzy_find, { desc = '[space] Fuzzily search in current buffer' })
     vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-    vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+    -- vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
