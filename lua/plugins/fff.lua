1643b63ed1e94a55df3c1b0cd98cb2354b2cb3f5
C Y K Sagar <cyksagar@protonmail.com>
2025-08-05 06:24:53 +0530

    sf -> fff nvim

--- lua/plugins/fff.lua
+++ lua/plugins/fff.lua
@@ -8,7 +8,7 @@ return {
   },
   keys = {
     {
-      "<leader>ff", -- try it if you didn't it is a banger keybinding for a picker
+      "<leader>sf", -- try it if you didn't it is a banger keybinding for a picker
       function()
         require("fff").find_files() -- or find_in_git_root() if you only want git files
       end,
