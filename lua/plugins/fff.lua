a9e786a64f2524d739f0b34929e82f2fb1ceeebd
C Y K Sagar <cyksagar@protonmail.com>
2025-08-08 01:41:02 +0530

    update fff

--- lua/plugins/fff.lua
+++ lua/plugins/fff.lua
@@ -5,6 +5,18 @@ return {
   -- build = "nix run .#release",
   opts = {
     -- pass here all the options
+    width = 0.97,          -- Window width as fraction of screen
+    height = 0.97,         -- Window height as fraction of screen
+    prompt = '> ',       -- Input prompt symbol
+    preview = {
+        enabled = true,
+        width = 0.8,
+        max_lines = 100,
+        max_size = 1024 * 1024, -- 1MB
+    },
+  title = 'FFF Files',  -- Window title
+  max_results = 10,     -- Maximum search results to display
+  max_threads = 16,      -- Maximum threads for fuzzy search
   },
   keys = {
     {
