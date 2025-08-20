730e715eae90c1497a5028a5b4889b4ea24c39d9
C Y K Sagar <cyksagar@protonmail.com>
2025-08-20 17:35:46 +0530

    update fff

--- lua/plugins/fff.lua
+++ lua/plugins/fff.lua
@@ -1,22 +1,20 @@
-return {
+return{
   "dmtrKovalenko/fff.nvim",
   build = "cargo build --release",
   -- or if you are using nixos
   -- build = "nix run .#release",
   opts = {
     -- pass here all the options
-    width = 0.97,          -- Window width as fraction of screen
-    height = 0.97,         -- Window height as fraction of screen
-    prompt = '> ',       -- Input prompt symbol
-    preview = {
-        enabled = true,
-        width = 0.8,
-        max_lines = 100,
-        max_size = 1024 * 1024, -- 1MB
+    max_threads =16,
+    max_results=10,
+    prompt = '> ',
+    layout = {
+      height = 0.9,
+      width = 0.9,
+      prompt_position = 'top', -- or 'top'
+      preview_position = 'right', -- or 'left', 'right', 'top', 'bottom'
+      preview_size = 0.75,
     },
-  title = 'FFF Files',  -- Window title
-  max_results = 10,     -- Maximum search results to display
-  max_threads = 16,      -- Maximum threads for fuzzy search
   },
   keys = {
     {
