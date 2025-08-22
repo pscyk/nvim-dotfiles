d5cc51c5369e0653bc8eaa06ead6778bf0c8cf92
C Y K Sagar <cyksagar@protonmail.com>
2025-08-22 16:25:43 +0530

    add lsps

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -2,6 +2,8 @@ vim.lsp.enable('basedpyright')
 vim.lsp.config('basedpyright', require('config.lsp.basedpyright'))
 vim.lsp.enable('ruff')
 vim.lsp.config('ruff', {})
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('rust_analyzer')
 
 vim.lsp.enable('pyrefly')
 vim.lsp.config('pyrefly', require('config.lsp.pyrefly'))
